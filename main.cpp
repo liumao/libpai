54424e131882e2634a68af1cc417a94533788ca3
makaloLau <loveyou12300liumao@163.com>
2022-03-31 18:02:34 +0800

    turn on/off led in thread

--- main.cpp
+++ main.cpp
@@ -1,6 +1,52 @@
-#include <iostream>
+#include <led.h>
+#include <script.h>
+
+/// \brief define pin
+#define LED_PIN_1 1
 
 int main(int argc, char* argv[]) {
-	std::cout << "Hello, pai" << std::endl;
+	/// init wiringpi 
+	wiringPiSetup();
+	
+	/// run flags
+	bool bRun = true;
+
+	/// init led module
+	Led led(LED_PIN_1);
+	
+	/// led thread
+	thread t([&]{
+		/// init script module
+		Script cmd;
+		
+		/// loop
+		while (bRun) {
+			/// turn on led
+			led.turnOn(500);
+			/// turn off led
+			led.turnOff(500);
+			
+			/// read temp
+			int nTemp = atoi(cmd.executeCMD("cat /sys/class/thermal/thermal_zone0/temp"));
+			cout << "cpu temp: " << nTemp / 1000 << endl;
+		}
+	});
+	t.detach();
+	
+	/// wait input
+	char input;
+	while(cin >> input) {
+		if (input == 'q') {
+			bRun = false;
+	
+			/// turn off led
+			led.turnOff(500);
+
+			break;
+		}
+		
+		/// sleep
+		delay(1000);
+	}
 	return 0;
 }
--- pai.h
+++ pai.h
@@ -27,6 +27,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <wiringPi.h>
 #include <iostream>
 #include <thread>
+#include <cstring>
 using namespace std;
 
 #endif //__PAI_H__
